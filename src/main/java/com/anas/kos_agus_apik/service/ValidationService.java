56df5d10529d85ddefc80a6e18ad96155056db7d
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-09-21 00:11:55 +0700

    18-9-2025

---
+++ src/main/java/com/anas/kos_agus_apik/service/ValidationService.java
@@ -0,0 +1,15 @@
+package com.anas.kos_agus_apik.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.xml.validation.Validator;
+
+@Service
+public class ValidationService {
+
+    @Autowired
+    private Validator validator;
+
+    public void validation
+}
